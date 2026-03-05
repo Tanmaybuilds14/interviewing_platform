6c38abf6b6059d7e4577c1151aa7576d0c255ae8
Tanmaybuilds14 <sarvetanmay276@gmail.com>
2026-03-05 18:23:35 +0530

    Database and tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import path from 'path';
-import { fileURLToPath } from 'url';
 import { ENV } from './lib/env.js';
+import { connectDB } from './lib/db.js';
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
+const __dirname=path.resolve();
 
 app.get("/health" ,(req,res)=>{
   res.status(200).json({msg:"success from health"});
@@ -16,16 +16,25 @@ app.get("/books" ,(req,res)=>{
 });
 
 if(ENV.NODE_ENV === "production"){
-  // Serve static files from frontend dist folder
-  const frontendDist = path.join(__dirname, "../../frontend/dist");
-  app.use(express.static(frontendDist));
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
-  // SPA fallback: serve index.html for all unmatched routes
-  app.get("/*" , (req,res)=>{
-    res.sendFile(path.join(frontendDist, "index.html"));
+  app.get("/{*any}" , (req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend", "dist" , "index.html"));
   });
 }
 
-const PORT = ENV.PORT || 3000;
-app.listen(PORT, ()=>console.log(`server is running on port ${PORT} https://localhost:${PORT}`));
+
+const Startserver = async() =>{
+  try {
+    await connectDB();
+    app.listen(3000, ()=>{
+    console.log(`server is running on port ${ENV.PORT} https://localhost:3000`);
+    connectDB();
+    });
+  } catch (error) {
+    console.error("error starting the server",error);
+  }
+}
+
+Startserver();
 
